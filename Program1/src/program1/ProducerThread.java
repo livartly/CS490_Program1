a121ef3f9ade2f95b6bb8da3f78749c2142c0686
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-02-22 15:59:59 -0600

    Added Producer Thread methods and variables

--- Program1/src/program1/ProducerThread.java
+++ Program1/src/program1/ProducerThread.java
@@ -3,15 +3,45 @@ package program1;
 // Producer Thread creates and adds processes to the heap
 // Thread wakes up at random periodic intervals and adds new processes to heap
 // Thread adds a total of 75 processes throughout its lifetime
-public class ProducerThread implements Runnable {
+public class ProducerThread {
+    /**
+     * The counter for the amount of nodes created.
+     */
+    private int nodeCount = 0;
+    /**
+     * The counter for the amount of times producer thread has awoken.
+     */
+    private int wakeUpCount = 0;
 
-
-    public ProducerThread () {
-
+    /**
+     * Gets the amount of nodes created.
+     *
+     * @return the node count.
+     */
+    public int getNodeCount () {
+        return nodeCount;
     }
 
-    @Override
-    public void run () {
+    /**
+     * Gets the amount of times producer has awoken.
+     *
+     * @return the wake up count.
+     */
+    public int wakeUpCount () {
+        return wakeUpCount;
+    }
 
+    /**
+     * Creates new instance of node.
+     * Generates random numbers to set priority, process id, and time slice
+     *
+     */
+    public int createNode () {
+        Node node = new Node();
+        node.setPriority(Utility.getRandomNumber(1,1000));
+        node.setProcessId(Utility.getRandomNumber(1,1000));
+        node.setTimeSlice(Utility.getRandomNumber(1,1000));
+        nodeCount++;
+        return 0;
     }
 }
