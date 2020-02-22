27d9db51a6946715ccdfdcf59e5ba11c1957f7da
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-02-22 15:14:59 -0600

    Added Producer Thread skeleton

---
+++ Program1/src/program1/ProducerThread.java
@@ -0,0 +1,17 @@
+package program1;
+
+// Producer Thread creates and adds processes to the heap
+// Thread wakes up at random periodic intervals and adds new processes to heap
+// Thread adds a total of 75 processes throughout its lifetime
+public class ProducerThread implements Runnable {
+
+
+    public ProducerThread () {
+
+    }
+
+    @Override
+    public void run () {
+
+    }
+}
