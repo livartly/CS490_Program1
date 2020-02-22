f9e057e09bed817c861f90aa34c11c61b80e8365
Liv Artly <37487751+livartly@users.noreply.github.com>
2020-02-22 16:47:50 -0600

    Added functions to ComsumerThread and Main

--- Program1/src/program1/ConsumerThread.java
+++ Program1/src/program1/ConsumerThread.java
@@ -1,14 +1,54 @@
 package program1;
 
-public class ConsumerThread implements Runnable {
+// Consumer thread requests nodes from the process queue
+// Simulate execution by sleeping for node slice duration
+// Report node's process ID, priority value, time when completed
+// Print report to screen
+public class ConsumerThread {
+
+	/**
+	 * Request node from process queue.
+	 *
+	 */
+	//public Node getNode () {
 
 
-	public ConsumerThread () {
+	//}
 
-	}
+	/**
+	 * Request number of nodes in queue.
+	 *
+	 */
+	//public int getQueueSize () {
+	//ProcessQueue queue = new ProcessQueue();
 
-	@Override
-	public void run () {
+	//}
 
-	}
+	/**
+	 * Get process queue
+	 *
+	 */
+	//public int getQueueSize () {
+		//ProcessQueue queue = new ProcessQueue();
+
+	//}
+
+	/**
+	 * Simulate execution by sleeping for node execution
+	 *
+	 */
+	//public int Sleep () {
+
+	//}
+
+	/**
+	 * Generates a report after a node has been consumed.
+	 * Prints report to screen
+	 * @return the report.
+	 */
+	//public String Report () {
+		//String nodeID =
+		//String report = "";
+		//return report;
+	//}
 }
